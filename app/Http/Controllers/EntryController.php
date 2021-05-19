d0602a6a8fb09f8b233ab362427da4495d048e03
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2021-05-20 00:26:07 +0200

    Add entry controller

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Entry;
 use Illuminate\Http\Request;
 
 class EntryController extends Controller
 {
-    //
+
+    public static function getEntries(int $bookId)
+    {
+        return Entry::where("book_id", $bookId)->get();
+    }
+
+    public static function addEntry(Request $request, int $bookId)
+    {
+        $entry = new Entry;
+
+        $entry->book_id = $bookId;
+        $entry->text = $request->text;
+        $entry->when = $request->when;
+        $entry->where = $request->where;
+        $entry->save();
+        return response(null, 201);
+    }
 }
